295806ebd2856d595caedeb219ae444b672e9501
moe-fun <michaeljmoughan@gmail.com>
2020-11-06 13:31:28 -0600

    Initial commit

---
+++ src/test/java/com/example/spring_mjm/SpringMjmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_mjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
